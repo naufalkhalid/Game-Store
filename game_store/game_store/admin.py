958027aa99086a61dbaacbb23d56a01c30a6382a
Gaurav Bhorkar <gaurav.bhorkar@aalto.fi>
2016-01-08 00:41:52 +0200

    untracking files

--- game_store/game_store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from game_store.models import UserProfile, Game, PlayerGame, ScoreBoard
-
-admin.site.register(UserProfile)
-admin.site.register(Game)
-admin.site.register(PlayerGame)
-admin.site.register(ScoreBoard)
